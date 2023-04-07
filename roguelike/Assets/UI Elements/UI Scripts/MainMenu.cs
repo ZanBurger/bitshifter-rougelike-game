0eebb153dafde8eeafccd4fbe7b875078e0b6e88
OzzanAr <ozan.arif@student.um.si>
2023-04-07 18:04:13 +0200

    Styled the UI and added a volume slider with functionality.

--- roguelike/Assets/UI Elements/UI Scripts/MainMenu.cs
+++ roguelike/Assets/UI Elements/UI Scripts/MainMenu.cs
@@ -5,14 +5,18 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public GameObject settingsPanel;
+
     public void StartGame()
     {
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
-
+    
     public void ExitGame()
     {
         Debug.Log("Game closed.");
         Application.Quit();
     }
+
+
 }
