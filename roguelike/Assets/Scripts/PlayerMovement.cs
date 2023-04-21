6f24d5dc8c3a153449f40b8b9038f3263eef190d
myiaw <82682849+myiaw@users.noreply.github.com>
2023-04-21 21:23:36 +0200

    added circle collider to player prefab, now it stops at obstacles

--- roguelike/Assets/Scripts/PlayerMovement.cs
+++ roguelike/Assets/Scripts/PlayerMovement.cs
@@ -1,25 +1,31 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed;
     private Vector2 _moveDirection;
-
     public Rigidbody2D rb;
 
     // Start is called before the first frame update
     void Start(){
         moveSpeed = 5;
-        
+
     }
 
     // Update is called once per frame, because of that we shouldn't do physics here.
-    void Update(){ 
+    void Update(){
         ProccesInputs();
     }
+
+    //Called at fixed intervals instead of users frame rate -> Better for physics.
+    private void FixedUpdate(){
+        Move();
+    }
+
     void ProccesInputs(){
         float moveX = Input.GetAxisRaw("Horizontal"); // Raw gives u either 0 or 1.
         float moveY = Input.GetAxisRaw("Vertical");
@@ -27,11 +33,6 @@ public class PlayerMovement : MonoBehaviour
         _moveDirection = new Vector2(moveX, moveY);
     }
 
-    
-    //Called at fixed intervals instead of users frame rate -> Better for physics.
-    private void FixedUpdate(){ 
-        Move();
-    }
     void Move(){
         rb.velocity = new Vector2(_moveDirection.x * moveSpeed, _moveDirection.y * moveSpeed);
     }
