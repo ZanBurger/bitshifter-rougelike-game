d9280f0ccafac2b0218d539a9dac158dbb5460c1
OzzanAr <ozan.arif@student.um.si>
2023-04-24 11:47:08 +0200

    Added a resolution dropdown menu

--- roguelike/Assets/UI Elements/UI Scripts/SettingsMenu.cs
+++ roguelike/Assets/UI Elements/UI Scripts/SettingsMenu.cs
@@ -2,19 +2,54 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class SettingsMenu : MonoBehaviour
 {
-   public AudioMixer audioMixer;
+    public AudioMixer audioMixer;
+    public Dropdown resolutionDropdown;
+        
+    Resolution[] resolutions;
 
-   public void SetVolume(float volume)
-   {
+    void Start()
+    {
+        resolutions = Screen.resolutions;
+
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        int currentResIndex = 0;
+        for(int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + "x" + resolutions[i].height;
+            options.Add(option);
+
+            if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResIndex = i;
+            }
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+
+    public void SetVolume(float volume)
+    {
         audioMixer.SetFloat("masterVolume", volume);
-   }
+    }
 
-   public void SetFullscreen(bool isFullscreen)
-   {
+    public void SetFullscreen(bool isFullscreen)
+    {
         Screen.fullScreen = isFullscreen;
-   }
+    }
+
+    public void SetResolution(int resIndex)
+    {
+        Resolution resolution = resolutions[resIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
 
 }
