f268fd48b214261b031113dd2acbdf1596afb35f
myiaw <82682849+myiaw@users.noreply.github.com>
2023-05-07 21:05:53 +0200

    multishot works

--- roguelike/Assets/Scripts/Player/Weapon.cs
+++ roguelike/Assets/Scripts/Player/Weapon.cs
@@ -10,6 +10,7 @@ public class Weapon : MonoBehaviour
     private float nextFireTime = 0f;
     private float currentFirerate;
     private IncreasedFirerate increasedFirerate;
+    bool equippedMultiShot = false;
 
     private void EquipIncreasedFirerate()
     {
@@ -25,8 +26,8 @@ public class Weapon : MonoBehaviour
         {
             firePoint = transform;
         }
-        currentFirerate = 0.1f;
-       EquipIncreasedFirerate();
+        currentFirerate = 0.25f; // Higher number = slower firerate
+       //EquipIncreasedFirerate();
     }
 
     void Update()
@@ -40,13 +41,36 @@ public class Weapon : MonoBehaviour
 
     private void InstantiateBullet()
     {
-        // Create the Bullet from the Bullet Prefab
-        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        if (equippedMultiShot)
+        {
+            // Create three bullets from the Bullet Prefab
+            GameObject bullet1 = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+            GameObject bullet2 = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+            GameObject bullet3 = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
 
-        // Add velocity to the bullet
-        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
-        rb.AddForce(firePoint.up * bulletForce, ForceMode2D.Impulse);
+            // Add velocity to the middle bullet
+            Rigidbody2D rb1 = bullet1.GetComponent<Rigidbody2D>();
+            rb1.AddForce(firePoint.up * bulletForce, ForceMode2D.Impulse);
+
+            // Add velocity to the right bullet
+            Rigidbody2D rb2 = bullet2.GetComponent<Rigidbody2D>();
+            rb2.AddForce(Quaternion.Euler(0, 0, -25) * firePoint.up * bulletForce, ForceMode2D.Impulse);
+
+            // Add velocity to the left bullet
+            Rigidbody2D rb3 = bullet3.GetComponent<Rigidbody2D>();
+            rb3.AddForce(Quaternion.Euler(0, 0, 25) * firePoint.up * bulletForce, ForceMode2D.Impulse);
+        }
+        else
+        {
+            // Create a single bullet from the Bullet Prefab
+            GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+
+            // Add velocity to the bullet
+            Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+            rb.AddForce(firePoint.up * bulletForce, ForceMode2D.Impulse);
+        }
     }
+
 
     public void SetCurrentFirerate(float newFirerate)
     {
