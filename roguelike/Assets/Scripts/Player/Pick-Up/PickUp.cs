dae4ece4bc8fdeb7dc466071288e39f6ce056bfe
zan <burger.zan@gmail.com>
2023-05-20 19:16:00 +0200

    Added a temporary function so that the 'increasedMovement' happens on player when he picks up an item

--- roguelike/Assets/Scripts/Player/Pick-Up/PickUp.cs
+++ roguelike/Assets/Scripts/Player/Pick-Up/PickUp.cs
@@ -6,6 +6,11 @@ public class PickUp : MonoBehaviour
 {
     void OnTriggerEnter2D(Collider2D other){
         if (other.gameObject.CompareTag("Player")){
+
+            Player.IncreasedMovement effect = other.gameObject.AddComponent<Player.IncreasedMovement>();
+
+            effect.playerController = other.GetComponent<PlayerController>();
+
             Destroy(gameObject);
         }
     }
