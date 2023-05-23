ab1a7044686fbd8853b298615102f9ba5862242b
OzzanAr <ozan.arif@student.um.si>
2023-05-23 12:54:40 +0200

    Added Game over screen

--- roguelike/Assets/Scripts/Helth.cs
+++ roguelike/Assets/Scripts/Helth.cs
@@ -2,12 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Helth : MonoBehaviour
 {
     public int livePoints = 1;
     public int maxHealth = 8;
 
     public Healthbar healthbar;
+    public GameObject gameOverUI;
 
     void Start()
     {
@@ -28,6 +30,8 @@ public class Helth : MonoBehaviour
         if (livePoints <= 0)
         {
             Destroy(gameObject);
+            Time.timeScale = 1f;
+            gameOverUI.SetActive(true);
         }
         if(healthbar != null)
         {
