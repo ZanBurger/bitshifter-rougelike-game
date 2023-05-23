706a08afefb8d4b4e065aa72926409097dff933a
myiaw <myiaw01@proton.me>
2023-05-23 12:45:48 +0200

    by picking up dropped item you can get increased movement speed now.

--- roguelike/Assets/Scripts/Player/Pick-Up/PickUp.cs
+++ roguelike/Assets/Scripts/Player/Pick-Up/PickUp.cs
@@ -4,12 +4,23 @@ using UnityEngine;
 
 public class PickUp : MonoBehaviour
 {
-    void OnTriggerEnter2D(Collider2D other){
-        if (other.gameObject.CompareTag("Player")){
-
-            Player.IncreasedMovement effect = other.gameObject.AddComponent<Player.IncreasedMovement>();
-
-            effect.playerController = other.GetComponent<PlayerController>();
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            // Generate a random number between 0 and 1 to determine the ability type
+            // int abilityType = Random.Range(0, 2);
+            int abilityType = 0;
+            switch (abilityType)
+            {
+                case 0:
+                    if (!other.gameObject.GetComponent<Player.IncreasedMovement>())
+                    {
+                        Player.IncreasedMovement increasedMovementEffect = other.gameObject.AddComponent<Player.IncreasedMovement>();
+                        increasedMovementEffect.playerController = other.GetComponent<PlayerController>();
+                    }
+                    break;
+            }
 
             Destroy(gameObject);
         }
